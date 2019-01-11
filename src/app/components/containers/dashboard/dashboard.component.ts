66685b77d38c35d46eb6e69388492787e94de433
Edward David Llanca Haro <ellanca@Macmini-de-ELLANCA.local>
2019-01-10 21:01:56 -0500

    add edit, save and remove element

--- src/app/components/containers/dashboard/dashboard.component.ts
+++ src/app/components/containers/dashboard/dashboard.component.ts
@@ -8,15 +8,18 @@ import { Component } from '@angular/core';
 export class DashboardComponent {
     public name: string = '';
     public desc: string = '';
+    public newName: string = '';
+    public newDesc: string = '';
     public items: any = new Array();
+    public showEdit: boolean;
+    private itemSelected: any;
     remove: boolean = false;
     saveItem() {
         if (this.name.length && this.desc.length) {
-            this.items.push({name: this.name, desc: this.desc});
+            return this.items.push({name: this.name, desc: this.desc});
         } else {
             alert('Ambos campos son requeridos.')
         }
-        console.log(this.items)
     }
 
     removeInfo() {
@@ -33,7 +36,38 @@ export class DashboardComponent {
         }
         console.log(this.items)
     }
-    editItem() {
-        console.log('entro editItem')
+    saveNewItem() {
+        if (this.itemSelected && this.itemSelected.name && this.itemSelected.desc) {
+            this.items.forEach((item, index) => {
+                if (this.itemSelected.name === item.name && this.itemSelected.desc === item.desc) {
+                    item.name = this.newName;
+                    item.desc = this.newDesc;
+                    this.items[index] = item;
+                    return this.showEdit = false;
+                }
+            })
+            this.newName = '';
+            this.newDesc = '';
+        }
+    }
+    editItem(data) {
+        const aux = {
+            name: '',
+            desc: ''
+        };
+        if (this.items.length && data && data.name && data.desc) {
+            this.items.forEach((item, index) => {
+                if (data.name === item.name && data.desc === item.desc) {
+                    this.itemSelected = {name: data.name, desc: data.desc};
+                    return this.showEdit = true
+                }
+            })
+        }
+
+    }
+    cancelNewItem() {
+      if (this.items.length && this.showEdit) {
+            return this.showEdit = false;
+        }
     }
 };
--- src/app/components/shared/card/card.component.ts
+++ src/app/components/shared/card/card.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 })
 export class CardComponent {
     @Output() valueChange = new EventEmitter();
+    @Output() valueChangeEdit = new EventEmitter();
     @Input() name: string; 
     @Input() desc: string;
     // @Input() removeItem: Function;
@@ -14,5 +15,8 @@ export class CardComponent {
 
     valueChanged() {
         this.valueChange.emit({name: this.name, desc: this.desc});
+    }
+    valueChangedEdit() {
+        this.valueChangeEdit.emit({name: this.name, desc: this.desc});
     }
 }
